a65eb25bc2a068461a62877256525b509f08b85e
PolinaEfimova888 <57696453+PolinaEfimova888@users.noreply.github.com>
2021-12-19 00:13:45 +0800

    Update main.lua

--- spiral/main.lua
+++ spiral/main.lua
@@ -1,1 +1,26 @@
+require("vector")
+require("flowmap")
+require("vehicle")
 
+
+function love.load()
+    width = love.graphics.getWidth()
+    height = love.graphics.getHeight()
+    flow = FlowMap:create(40)
+    flow:init()
+
+    vehicle = Vehicle:create(width/2, height/2)
+    vehicle.velocity.x = 1
+    vehicle.velocity.y = 1.5
+end
+
+function love.update(dt)
+    vehicle:borders()
+    vehicle:update()
+    vehicle:follow(flow)
+end
+
+function love.draw()
+    flow:draw()
+    vehicle:draw()
+end
